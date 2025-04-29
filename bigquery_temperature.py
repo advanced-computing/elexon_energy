8f5d1a4a3d1eb1e38fd505683453e3687a25ab2e
Ijaz Ahmed Khan <ik2557@columbia.edu>
2025-04-28 20:42:27 -0400

    Update bigquery_temperature.py

--- bigquery_temperature.py
+++ bigquery_temperature.py
@@ -23,24 +23,12 @@ SCOPES = ['https://www.googleapis.com/auth/cloud-platform',
 
 # === AUTHENTICATION ===
 def get_bq_credentials():
-    bq_credentials = {
-        "type": st.secrets["gcp_service_account"]["type"],
-        "project_id": st.secrets["gcp_service_account"]["project_id"],
-        "private_key_id": st.secrets["gcp_service_account"]["private_key_id"],
-        "private_key": st.secrets["gcp_service_account"]["private_key"],
-        "client_email": st.secrets["gcp_service_account"]["client_email"],
-        "client_id": st.secrets["gcp_service_account"]["client_id"],
-        "auth_uri": st.secrets["gcp_service_account"]["auth_uri"],
-        "token_uri": st.secrets["gcp_service_account"]["token_uri"],
-        "auth_provider_x509_cert_url": st.secrets["gcp_service_account"]["auth_provider_x509_cert_url"],
-        "client_x509_cert_url": st.secrets["gcp_service_account"]["client_x509_cert_url"],
-        "universe_domain": st.secrets["gcp_service_account"]["universe_domain"]
-    }
+    import json
+    bq_credentials = json.loads(st.secrets["ELEXON_SECRETS_AS"])
     credentials = service_account.Credentials.from_service_account_info(
         bq_credentials, scopes=SCOPES
     )
     return credentials
-
 
 def check_table_exists(table_name, credentials):
     """Check if a table exists in BigQuery"""
